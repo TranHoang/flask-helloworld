9dffc95baca1687444ca79129083afb3453259da
Tran Hoang <tran.hoangnguyen@asnet.com.vn>
2018-09-10 14:38:26 -0400

    Add TimestampModel and flask-bcrypt module.

--- app-api/core/models.py
+++ app-api/core/models.py
@@ -1,3 +1,12 @@
+from datetime import datetime
+
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 db = SQLAlchemy()
+bcrypt = Bcrypt()
+
+class TimestampMixin(object):
+    created = db.Column(
+        db.DateTime, nullable=False, default=datetime.utcnow)
+    updated = db.Column(db.DateTime, onupdate=datetime.utcnow)
