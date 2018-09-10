20b01cfa38486acbc3ebad7fdf4999ba84eb98c6
Tran Hoang <tran.hoangnguyen@asnet.com.vn>
2018-09-10 14:46:27 -0400

    Register User and ToDo Resource to the application.

--- app-api/manage.py
+++ app-api/manage.py
@@ -1,10 +1,13 @@
 import os
 
 from flask import Flask
+from flask_restful import Api
 from flask_migrate import Migrate
 
-from core import db
-from user import user_api
+
+from core import db, bcrypt
+from user import UserResource, UserListResource
+from todo import ToDoResource
 
 app = Flask(__name__)
 
@@ -15,13 +18,23 @@ app_settings = os.getenv(
 )
 app.config.from_object(app_settings)
 
+# Create API
+api = Api(app)
+
 # Sync up database
 db.app = app
 db.init_app(app)
 
+bcrypt.init_app(app)
+
 migrate = Migrate(app, db) # this
 
-app.register_blueprint(user_api, url_prefix='/users')
+##
+## Setup the Api resource routing here
+##
+api.add_resource(UserResource, '/user/<user_id>')
+api.add_resource(UserListResource, '/users')
+api.add_resource(ToDoResource, '/todo/<id>')
 
 if __name__ == '__main__':
     app.run()
