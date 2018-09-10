59ae8526f754bc900cd99e0019680fbb0ef45474
Tran Hoang <tran.hoangnguyen@asnet.com.vn>
2018-09-10 14:41:14 -0400

    Update User model.

--- app-api/user/models.py
+++ app-api/user/models.py
@@ -1,10 +1,30 @@
-from core.models import db
+from flask_sqlalchemy import BaseQuery
+
+from core.models import (
+    db,
+    bcrypt,
+    TimestampMixin
+)
 
 
-class User(db.Model):
+class UserQuery(BaseQuery):
+    def get_by_id(self, id):
+        return self.get(ident=id)
+
+    def get_by_email(self, email):
+        return self.filter_by(email=email).first()
+
+class User(db.Model, TimestampMixin):
+    query_class = UserQuery
     id = db.Column(db.Integer, primary_key=True)
+    todos = db.relationship('Todo', backref='user', lazy=True)
+
+    email = db.Column(db.String(255), unique=True, nullable=False)
+    password_hash = db.Column(db.String(100))
+
     first_name = db.Column(db.Unicode(100))
     last_name = db.Column(db.Unicode(100))
+
     auth_sub = db.Column(db.String(100))
     notifications_enabled = db.Column(db.Boolean())
     notifications_radius_meters = db.Column(db.Float())
@@ -12,5 +32,30 @@ class User(db.Model):
     email = db.Column(db.String(100))
     avatar = db.Column(db.String(100))
     expires_at = db.Column(db.DateTime())
-    created_at = db.Column(db.DateTime())
-    updated_at = db.Column(db.DateTime())
+
+    @property
+    def password(self):
+        raise AttributeError('password is a write only field')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = bcrypt.generate_password_hash(password).decode('utf-8')
+
+    def check_password(self, password):
+        return bcrypt.check_password_hash(self.password_hash, password)
+
+    @classmethod
+    def create_new_user(cls, data):
+        user = User.query.get_by_email(email=data['email'])
+        if not user:
+            new_user = User(
+                email=data['email'],
+                password=data['password'],
+                first_name=data['first_name'],
+                last_name=data['last_name']
+            )
+
+            db.session.add(new_user)
+            db.session.commit()
+
+        return user
