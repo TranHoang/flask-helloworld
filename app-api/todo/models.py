232fbb352cd8e16680919fd170e6df60b44edd33
Tran Hoang <tran.hoangnguyen@asnet.com.vn>
2018-09-11 11:49:02 -0400

    Update Todo queryset and saving model.

--- app-api/todo/models.py
+++ app-api/todo/models.py
@@ -10,8 +10,31 @@ from core.models import (
 #################################################
 
 class TodoQuery(BaseQuery):
-    def get_by_id(self, id):
-        return self.get(ident=id)
+
+    def completed(self):
+        """
+        Filter completed todos
+        """
+        return self.filter_by(completed=True)
+
+    def uncompleted(self):
+        """
+        Filter uncompleted todos
+        """
+        return self.filter_by(completed=False)
+
+    def by_owner_and_id(self, user_id, id):
+        """
+        Filter Todo by owner and todo id
+        """
+        return self.by_owner(user_id) \
+            .by_id(id)
+
+    def by_owner(self, user_id):
+        return self.filter_by(user_id=user_id)
+
+    def by_id(self, id):
+        return self.filter_by(id=id)
 
 class Todo(db.Model, TimestampMixin):
     query_class = TodoQuery
@@ -23,5 +46,20 @@ class Todo(db.Model, TimestampMixin):
         nullable=False)
     title = db.Column(db.Unicode(100))
     due_date = db.Column(db.DateTime())
-    completed = db.Column(db.Boolean())
+    completed = db.Column(db.Boolean(), default=False)
     completed_date = db.Column(db.DateTime())
+
+    @classmethod
+    def create_todo(cls, data):
+        todo = Todo(**data)
+        db.session.add(todo)
+        db.session.commit()
+        return todo
+
+    def update(self, data):
+        for key,value in data.items():
+            setattr(self, key, value)
+        db.session.add(self)
+        db.session.commit()
+        return self
+
