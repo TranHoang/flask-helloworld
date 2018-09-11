cc7dd3c530e394e1268e0bb1e74eac982b02ff86
Tran Hoang <tran.hoangnguyen@asnet.com.vn>
2018-09-11 11:57:31 -0400

    Setup configration variable: JWT

--- app-api/config.py
+++ app-api/config.py
@@ -1,4 +1,6 @@
 import os
+from datetime import timedelta
+
 postgres_uri = 'postgresql://postgres:123456@localhost/api'
 
 class BaseConfig():
@@ -7,16 +9,23 @@ class BaseConfig():
     """
     DEBUG = False
     SECRET_KEY = os.getenv('SECRET_KEY', 'invisible_key')
+    JWT_SECRET_KEY = 'super-secret'
+    # Config JWT expires time in minutes
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(minutes=os.getenv('JWT_ACCESS_TOKEN_EXPIRES', 120))
 
 
 class DevelopmentConfig(BaseConfig):
+    """
+    Configuration for development env
+    """
+
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', "sqlite:///db.sqlite3")
 
 
 class ProductionConfig(BaseConfig):
     """
-    Production configuration
+    Configuration for production env
     """
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', postgres_uri)
