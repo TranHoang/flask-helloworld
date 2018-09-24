216727e16f455ac6c9bec155581695aba4f84b0c
Tran Hoang <tran.hoangnguyen@asnet.com.vn>
2018-09-24 16:50:45 -0400

    Return 401 when login failed.

--- src/auth/apis.py
+++ src/auth/apis.py
@@ -1,4 +1,4 @@
-from flask import jsonify
+from flask import abort
 from flask_jwt_extended import create_access_token
 from flask_restful import (
     Resource,
@@ -24,6 +24,8 @@ class AuthenticationResource(Resource):
         if user and user.check_password(data['password']):
             access_token = create_access_token(identity=user.id)
             return {'access_token': access_token}
+        else:
+            abort(401)
 
 
 # Register API
