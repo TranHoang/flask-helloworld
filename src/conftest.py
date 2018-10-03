e7d12028643b8349643b9ca4a02a20c282255e18
Tran Hoang <tran.hoangnguyen@asnet.com.vn>
2018-10-03 17:18:08 -0400

    Setup flask client for API testing by using pytest fixture.

---
+++ src/conftest.py
@@ -0,0 +1,31 @@
+import os
+
+import pytest
+import flask_migrate
+
+from core import app
+from todo.models import Todo
+
+
+@pytest.fixture(scope="session")
+def client():
+    """
+    Setup flask client in session scope to test API
+    """
+
+    # Delete test db file
+    db_path = "./db_test.sqlite3"    
+    if os.path.exists(db_path):
+        os.remove(db_path)
+
+    # Create test db file and migration
+    uri = "sqlite:///%s" % db_path
+    app.config['SQLALCHEMY_DATABASE_URI'] = uri
+
+    app.config['TESTING'] = True
+    client = app.test_client()
+
+    with app.app_context():
+        flask_migrate.upgrade(directory="src/migrations")
+
+    return client
